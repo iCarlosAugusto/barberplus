85035aab9f98384b7aa33933d57331a710c564e3
iCarlosAugusto <augusto.dev@proton.me>
2025-03-02 12:33:27 -0300

    feat: :sparkles: ajustando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import "./App.css";
 import { Routes, Route } from 'react-router-dom'
-import Navbar from "./components/Navbar";
-import EmployeesPage from "./pages/EmployeesPage";
-import { ServicesPage } from "./pages/ServicesPage";
-
-// Vamos criar um componente Home básico para a página inicial
-const Home = () => (
-  <div>
-    <h1>Página Inicial</h1>
-    <p>Bem-vindo ao nosso sistema de agendamento!</p>
-  </div>
-);
-
-// Componente para página não encontrada
-const NotFound = () => (
-  <div>
-    <h1>404 - Página não encontrada</h1>
-    <p>A página que você está procurando não existe.</p>
-  </div>
-);
+import Navbar from "@/components/Navbar";
+import EmployeesPage from "@/pages/EmployeesPage";
+import { ServicesPage } from "@/pages/ServicesPage";
+import { NotFoundPage } from "@/pages/NotFoundPage";
 
 function App() {
 
@@ -26,10 +11,9 @@ function App() {
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/" element={<ServicesPage />} />
         <Route path="/employees" element={<EmployeesPage />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
   );
