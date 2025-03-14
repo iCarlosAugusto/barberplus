a92480abcd7f91c7667faaca2fe60ff4eba9ad77
iCarlosAugusto <augusto.dev@proton.me>
2025-03-14 17:19:42 -0300

    feat: :sparkles: melhorado na forma de buacar funcionariso disponiveis

--- src/components/ModalBookingFlow/AvailableEmployees.tsx
+++ src/components/ModalBookingFlow/AvailableEmployees.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import { api } from "@/http/request";
+import { useBookingStore } from "@/store/bookingStore";
+import { formatDate } from "@/utils/formatDate";
+import { useEffect } from "react";
+
 
 export function AvailableEmployees() {
-  return (
-    <div>AvailableEmployees</div>
-  )
+  const { currentJobChangeEmployee, selectedJobs } = useBookingStore();
+
+  const currentJob = selectedJobs.find((job) => job.job.id === currentJobChangeEmployee?.id);
+
+  const fetchAvailableTimeByEmployee = async () => {
+
+    const employees = currentJob?.job.doneByEmployees;
+    
+    if (!employees) return;
+    for (const employee of employees) {
+      try {
+        const { data } = await api.get(`/employees/${employee.id}/time-slots?date=${formatDate(currentJob?.date)}`);
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+
+
+  useEffect(() => {
+    fetchAvailableTimeByEmployee();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  return <>
+    <p>{JSON.stringify(currentJob)}</p>
+    {currentJobChangeEmployee?.doneByEmployees.map((employee) => (
+      <div key={employee.id}>
+        <h3>{employee.name}</h3>
+      </div>
+    ))}
+  </>;
 }
