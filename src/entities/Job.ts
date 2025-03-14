cb4d1fcc4a8ddc64ef82099ccd749a09c8d71c83
iCarlosAugusto <augusto.dev@proton.me>
2025-03-14 17:20:38 -0300

    feat: :sparkles: modificado entidade

--- src/entities/Job.ts
+++ src/entities/Job.ts
@@ -1,7 +1,10 @@
+import { Employee } from "./Employee";
+
 export interface Job {
     id: string;
     name: string;
     description: string;
     price: number;
     durationMinutes: number;
+    doneByEmployees: Employee[];
 }
