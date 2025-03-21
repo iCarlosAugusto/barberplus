2d40d968e92c988d00fc503b46e979123d6fc6b7
iCarlosAugusto <augusto.dev@proton.me>
2025-03-20 23:10:53 -0300

    feat: :sparkles: melhorado store

--- src/store/companyStore.ts
+++ src/store/companyStore.ts
@@ -1,16 +1,22 @@
 import { Company } from '@/entities/Company';
+import { Job } from '@/entities/Job';
 import { create } from 'zustand';
 
 interface CompanyState {
   company: Company | null;
+  companyJobs: Job[];
+  setCompanyJobs: (jobs: Job[]) => void;
   setCompany: (company: Company) => void;
   clearCompany: () => void;
 }
 
 export const useCompanyStore = create<CompanyState>((set) => ({
   company: null,
-  
+  companyJobs: [],
   setCompany: (company) => set({ company }),
   
   clearCompany: () => set({ company: null }),
+
+  setCompanyJobs: (jobs) => set({ companyJobs: jobs }),
+
 }));
