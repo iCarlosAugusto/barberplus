6f6abc09d374db8f0c5ee7bdbb9d8c4281f1b1fa
iCarlosAugusto <augusto.dev@proton.me>
2025-03-20 22:26:57 -0300

    refactor store

--- src/store/bookingStore.ts
+++ src/store/bookingStore.ts
@@ -1,46 +1,86 @@
+import { Employee } from '@/entities/Employee';
 import { Job } from '@/entities/Job';
 import { JobSchedule } from '@/entities/JobSchedule';
 import { create } from 'zustand';
 
-// Define the types for our store
 export type BookingContent = 'home' | 'services' | 'employees' | 'confirmation';
+
+interface AddJobProps {
+  job: Job;
+  employee: Employee | null;
+  startTime: Date;
+  endTime: Date;
+}
 
 interface BookingState {
   currentContent: BookingContent;
-  selectedJobs: JobSchedule[];
+  jobSchedule: JobSchedule | null;
   setCurrentContent: (content: BookingContent) => void;
-  addJob: (job: JobSchedule) => void;
+  addJob: (props: AddJobProps) => void;
   removeJob: (jobId: string) => void;
-  clearJobs: () => void;
+  clearJobs: () => void;  
   goToServices: () => void;
   goToHome: () => void;
   goToEmployees: () => void;
   goToConfirmation: () => void;
   currentJobChangeEmployee: null | Job;
   setCurrentJobChangeEmployee: (job: Job) => void;
+  clearCurrentJobChangeEmployee: () => void;
 }
 
-// Create the store
 export const useBookingStore = create<BookingState>((set) => ({
   currentContent: 'home',
+  jobSchedule: null,
   selectedJobs: [],
   currentJobChangeEmployee: null,
   
   setCurrentContent: (content) => set({ currentContent: content }),
   
-  // Add a service to the selected services
-  addJob: (job) => 
-    set((state) => ({
-      selectedJobs: [...state.selectedJobs, job]
-    })),
+  addJob: ({job, employee, startTime, endTime}: AddJobProps ) => 
+    set((state) => {
+      if (!state.jobSchedule) {
+        // Criar um novo JobSchedule quando não existe
+        return { 
+          jobSchedule: {
+            id: crypto.randomUUID(),
+            date: new Date(),
+            jobs: [{
+              job: job, 
+              employee: employee, 
+              startTime: startTime,
+              endTime: endTime
+            }]
+          }
+        };
+      }
+      
+      return {
+        jobSchedule: {
+          ...state.jobSchedule,
+          jobs: [...state.jobSchedule.jobs, {
+            job: job, 
+            employee: employee, 
+            startTime: startTime,
+            endTime: endTime
+          }]
+        }
+      };
+    }),
 
   removeJob: (jobId) => 
-    set((state) => ({
-      selectedJobs: state.selectedJobs.filter(job => job.id !== jobId)
-    })),
+    set((state) => {
+      if (!state.jobSchedule) return { jobSchedule: null };
+      
+      return {
+        jobSchedule: {
+          ...state.jobSchedule,
+          jobs: state.jobSchedule.jobs.filter(job => job.job.id !== jobId)
+        }
+      };
+    }),
   
   // Clear all selected services
-  clearJobs: () => set({ selectedJobs: [] }),
+  clearJobs: () => set({ jobSchedule: null }),
 
   setCurrentJobChangeEmployee: (job: Job) => set({ currentJobChangeEmployee: job }),
   
