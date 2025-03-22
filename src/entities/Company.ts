f53c2eb25e4e4aaaa6d30832c6bdcac3fc745b6b
iCarlosAugusto <augusto.dev@proton.me>
2025-03-22 01:16:22 -0300

    feat: :sparkles: adicionado weekSchedule

--- src/entities/Company.ts
+++ src/entities/Company.ts
@@ -1,3 +1,5 @@
+import { WorkSchedule } from "@/entities/WorkSchedule";
+
 export interface Company {
     id: string;
     name: string;
@@ -8,4 +10,5 @@ export interface Company {
     email: string;
     createdAt: string;
     updatedAt: string;
+    workSchedule: WorkSchedule[];
 }
