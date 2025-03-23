d83729d5457b6de00eac0eeedf9775ecade52d24
iCarlosAugusto <augusto.dev@proton.me>
2025-03-23 20:56:19 -0300

    feat: :sparkles: adicionado trycatch

--- src/utils/addMinutesToTime.tsx
+++ src/utils/addMinutesToTime.tsx
@@ -1,9 +1,19 @@
 import { parse, addMinutes, format } from "date-fns";
 
 export function addMinutesToTime(initialTime: string, addTime: string): string {
-    const baseTime = parse(initialTime, "HH:mm", new Date());
-    const [addHours, addMinutesValue] = addTime.split(":").map(Number);
-    const newTime = addMinutes(baseTime, addHours * 60 + addMinutesValue);
-    return format(newTime, "HH:mm");
+    let baseTime;
+
+    try {
+        baseTime = parse(initialTime, "HH:mm", new Date());
+        const [addHours, addMinutesValue] = addTime.split(":").map(Number);
+        const newTime = addMinutes(baseTime, addHours * 60 + addMinutesValue);
+        return format(newTime, "HH:mm");
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+        baseTime = parse(initialTime, "HH:mm:ss", new Date());
+        const [addHours, addMinutesValue] = addTime.split(":").map(Number);
+        const newTime = addMinutes(baseTime, addHours * 60 + addMinutesValue);
+        return format(newTime, "HH:mm:ss");
+    }
 }
 
